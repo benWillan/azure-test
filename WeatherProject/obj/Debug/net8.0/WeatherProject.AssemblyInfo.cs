954a5246abe4217bd43b18e27e72220fba50e8b7
Ben Willan <93150547+benWillan@users.noreply.github.com>
2025-10-30 19:24:31 +0000

    [chore] added build files

--- WeatherProject/obj/Debug/net8.0/WeatherProject.AssemblyInfo.cs
+++ WeatherProject/obj/Debug/net8.0/WeatherProject.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WeatherProject")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+99e948c11431f47fc4fc9962ae973c1ce08bdb42")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9958af131187fc793e43825abe63dbe87aa0c1a6")]
 [assembly: System.Reflection.AssemblyProductAttribute("WeatherProject")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WeatherProject")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
